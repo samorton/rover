1e98005087a53f3ad22eb9a6e4d920d0e28a2cc5
Sam Orton <sam.orton@betfair.com>
2017-04-21 11:58:39 +0100

    initial commit

---
+++ rover/StartupTask.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Windows.ApplicationModel.Background;
+using Windows.Devices.Gpio;
+using Windows.System.Threading;
+
+// The Background Application template is documented at http://go.microsoft.com/fwlink/?LinkID=533884&clcid=0x409
+
+namespace rover {
+    public sealed class StartupTask : IBackgroundTask {
+        BackgroundTaskDeferral _deferral;
+        private GpioPinValue value = GpioPinValue.High;
+        private const int LED_PIN = 5;
+        private GpioPin pin;
+        private ThreadPoolTimer timer;
+
+        public void Run(IBackgroundTaskInstance taskInstance) {
+            _deferral = taskInstance.GetDeferral();
+            InitGPIO();
+            timer = ThreadPoolTimer.CreatePeriodicTimer(Timer_Tick, TimeSpan.FromMilliseconds(500));
+        }
+
+        private void InitGPIO() {
+            pin = GpioController.GetDefault().OpenPin(LED_PIN);
+            pin.Write(GpioPinValue.High);
+            pin.SetDriveMode(GpioPinDriveMode.Output);
+        }
+
+        private void Timer_Tick(ThreadPoolTimer timer) {
+            value = value == GpioPinValue.High ? GpioPinValue.Low : GpioPinValue.High;
+            pin.Write(value);
+        }
+
+        public void Stop() {
+            _deferral?.Complete();
+        }
+    }
+}
---
+++ rover/Test.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rover
+{
+    class Test {
+
+
+        private const int LedPin = 12;
+
+        public void FLashLED() {
+            
+
+        }
+    }
+}
